99aeeff6a317107f9f56d002e2257c6258a94f9e
Animesh Thomas <animeshthomas262@gmail.com>
2024-07-19 19:35:31 +0530

    Index Page Done

---
+++ client-expense-app/src/Components/Index/Index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+const Index = () => {
+  return (
+    <div className="home-container">
+      <main className="home-main text-center">
+        <div className="intro">
+          <h2>Welcome to your Expense Tracker</h2>
+          <p>Keep track of your expenses easily and efficiently.</p>
+          <button className="btn signup-btn">Signup/Login</button>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default Index;
