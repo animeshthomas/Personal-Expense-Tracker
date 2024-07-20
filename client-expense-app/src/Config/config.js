c29cffc781adf9d423f032fc48cf8ee69cd7c8c5
Animesh Thomas <animeshthomas262@gmail.com>
2024-07-20 21:17:30 +0530

    Added Deployed Backend Link

--- client-expense-app/src/Config/config.js
+++ client-expense-app/src/Config/config.js
@@ -1,4 +1,5 @@
-let BASE_URL = 'http://localhost:8000';
+//let BASE_URL = 'http://localhost:8000';
+let BASE_URL = 'https://personal-expense-tracker-09we.onrender.com/';
 
 module.exports = {
   BASE_URL,
